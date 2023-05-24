6498cb2a63cd1c0ec6c8b521472698f67d4d9237
Ruckshan <ruckshan1024@gmail.com>
2023-05-25 00:36:41 +0530

    CORS Error fixed 3

--- backend/utils/hashToken.js
+++ backend/utils/hashToken.js
@@ -8,7 +8,7 @@ const hashToken = (res, userId) => {
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+        secure: false, // Use secure cookies in production
         sameSite: 'strict', // Prevent CSRF attacks
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
