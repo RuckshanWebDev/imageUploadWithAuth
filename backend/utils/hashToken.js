72d4a032579090533e4efeedfe365cb6a49783de
Ruckshan <ruckshan1024@gmail.com>
2023-05-25 19:39:09 +0530

    CORS fix 8

--- backend/utils/hashToken.js
+++ backend/utils/hashToken.js
@@ -8,7 +8,7 @@ const hashToken = (res, userId) => {
 
     res.cookie('jwt', token, {
         domain: process.env.FRONTEND_URL,
-        httpOnly: false,
+        httpOnly: true,
         secure: false, // Use secure cookies in production
         sameSite: 'lax', // Prevent CSRF attacks
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
